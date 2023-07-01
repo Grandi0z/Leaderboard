eae714d66aae201b643459c3efab940a17dfe905
Grandi0z <josephlkhausi@gmail.com>
2023-07-01 20:47:53 +0200

    Change UI

--- src/modules/viewFunction.js
+++ src/modules/viewFunction.js
@@ -13,13 +13,49 @@ const displayMessage = (msg) => {
     message.classList.add('hidden');
   }, 2000);
 };
+
+// this method will be use in a future feature
+const deleteScore = async (id) => {
+  const scores = await getScore();
+  // I want to use a const array but I cant because I'll neeed to change it value
+  const newScores = { value: [] };
+  newScores.value = scores.result.filter((score) => !score.user.includes(id));
+};
+
 const renderScore = (scores) => {
   table.innerHTML = '';
   scores.forEach((score) => {
+    const btnDelete = document.createElement('button');
     const tr = table.insertRow();
     const td = tr.insertCell(0);
-    const tdContent = `${score.user} : ${score.score}`;
-    td.innerHTML = tdContent;
+    const td1 = tr.insertCell(1);
+    const [UserName, userId] = score.user.split('_');
+    btnDelete.innerHTML = `<i class="bi bi-trash3-fill trash" id=${userId}></i>`;
+    const tdContent = { value: '' };
+    if (score.score > 10) {
+      tdContent.value = `<i class="bi bi-person-circle"></i> ${UserName} : ${score.score}  <i class="bi bi-arrow-up-right-circle-fill"></i>`;
+    } else if (score.score < 10) {
+      tdContent.value = `<i class="bi bi-person-circle"></i> ${UserName} : ${score.score}  <i class="bi bi-arrow-down-right-circle-fill"></i>`;
+    } else {
+      tdContent.value = `<i class="bi bi-person-circle"></i> ${UserName} : ${score.score}  <i class="bi bi-arrow-right-circle-fill"></i>`;
+    }
+
+    td.innerHTML = tdContent.value;
+    td1.appendChild(btnDelete);
+    btnDelete.addEventListener('click', (e) => {
+      deleteScore(e.target.id);
+      if (e.target.id) {
+        e.target
+          .parentElement
+          .parentElement.parentElement.parentElement
+          .removeChild(e.target.parentElement.parentElement.parentElement);
+      } else {
+        e.target
+          .parentElement
+          .parentElement.parentElement
+          .removeChild(e.target.parentElement.parentElement);
+      }
+    });
   });
 };
 const addScore = async () => {
@@ -31,8 +67,8 @@ const addScore = async () => {
 };
 
 const refreshScore = async () => {
-  const score = await getScore();
-  renderScore(score.result);
+  const scores = await getScore();
+  renderScore(scores.result);
 };
 
 export { addScore, refreshScore };
